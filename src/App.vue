b2562580c6af62276430f5cd52d2d23d65532991
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-02-03 09:28:09 +0800

    feat: remove extra styles

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,3 @@ export default {
   },
 };
 </script>
-
-<style>
-/* stylelint-disable */
-#app {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'PingFang SC',
-    'Hiragino Sans GB', 'Microsoft YaHei', Avenir, 'Helvetica Neue', Helvetica,
-    Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-</style>
