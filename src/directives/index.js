baa4a8c7ab4672b54cdd05f61a22cfbf955c8b80
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-02-03 09:24:35 +0800

    feat: remove directives folder

--- src/directives/index.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue';
-import directiveResize from './resize';
-
-Vue.directive(directiveResize.name, directiveResize);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue';
-import '@/directives';
 import '@/plugins';
 import i18n from './i18n';
 import router from './router';
