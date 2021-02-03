6eacfbb8af985d0927c29b756db8d4666441b20c
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-02-03 09:50:57 +0800

    perf: remove console.log

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,6 @@ export default {
   mounted() {
     this.$i18n.locale = this.$store.state.language;
     this.$vuetify.theme.dark = this.$store.state.theme === 'dark';
-    console.log('i18n', this.$i18n);
   },
 };
 </script>
