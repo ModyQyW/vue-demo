b3f3a56212d9b8133060d1736bd9335c43f04b6b
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-04-26 10:38:08 +0800

    feat: remove jest

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
-  moduleNameMapper: {
-    '@/(.*)$': '<rootDir>/src/$1',
-  },
-  transformIgnorePatterns: ['/node_modules/(?!lodash-es)'],
-};
